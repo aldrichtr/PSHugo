615d8c05f5948273328e3d49a8382cd5a9082cf3
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2022-04-29 16:24:49 -0400

    feat: updated get section to return all sections if no path is specified

--- source/PSHugo/public/Get-HugoSection.ps1
+++ source/PSHugo/public/Get-HugoSection.ps1
@@ -8,7 +8,6 @@ function Get-HugoSection {
     param(
         # The path to the file
         [Parameter(
-            Mandatory,
             ValueFromPipeline
         )]
         [Alias('PSPath')]
@@ -18,25 +17,32 @@ function Get-HugoSection {
         Write-Debug "Begin $($MyInvocation.MyCommand.Name)"
     }
     process {
-        try {
-            if ($Path -is [string]) {
-                $file = Get-Item $Path
-            } elseif ($Path -is [System.IO.FileInfo]) {
-                $file = $Path
+        if ($PSBoundParameters['Path']) {
+            try {
+                if ($Path -is [string]) {
+                    $file = Get-Item $Path
+                } elseif ($Path -is [System.IO.FileInfo]) {
+                    $file = $Path
+                }
+            } catch {
+                Write-Error "Could not resolve path '$Path'`n$_"
             }
-        } catch {
-            Write-Error "Could not resolve path '$Path'`n$_"
+            $hugo_root = Resolve-HugoRoot $file
+            $content_root = Join-Path $hugo_root 'content' -Resolve
+            Write-Debug "Content root: $content_root"
+            $rel_path = $file.FullName -replace [regex]::Escape($content_root) , ''
+            Write-Debug "relative path is $rel_path"
+            # the rel_path has the first directory separator still on it, so when we split it
+            # into parts, the first one is blank
+            # /path/to/file = @( '', 'path', 'to', 'file')
+            # so we want to take the next one, at index 1
+            $section = ($rel_path -split [regex]::Escape([IO.Path]::DirectorySeparatorChar))[1]
+        } else {
+            $hugo_root = Resolve-HugoRoot
+            $content_root = Join-Path $hugo_root 'content' -Resolve
+            Write-Debug "Content root: $content_root"
+            $section = Get-ChildItem -Path $content_root -Directory | Select-Object 'Name' -ExpandProperty 'Name'
         }
-        $hugo_root = Resolve-HugoRoot $file
-        $content_root = Join-Path $hugo_root 'content' -Resolve
-        Write-Debug "Content root: $content_root"
-        $rel_path = $file.FullName -replace [regex]::Escape($content_root) , ''
-        Write-Debug "relative path is $rel_path"
-        # the rel_path has the first directory separator still on it, so when we split it
-        # into parts, the first one is blank
-        # /path/to/file = @( '', 'path', 'to', 'file')
-        # so we want to take the next one, at index 1
-        $section = ($rel_path -split [regex]::Escape([IO.Path]::DirectorySeparatorChar))[1]
     }
     end {
         $section
